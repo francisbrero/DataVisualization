395f56ad6953ca4a0e39cf934e95d154992c4cb4
Francis-a1 <francis.brero@agilone.com>
2014-09-30 22:11:41 -0700

    New data

--- Code.sql
+++ Code.sql
@@ -1,12 +1,15 @@
-SELECT	Client = 'AMHobbies',
+SELECT	Decile = [L12MDecile],
+		Client = 'AMH',		
 		DateY = YEAR(df_transactionDate),
 		DateM = MONTH(df_transactionDate),
 		Buyers = COUNT(DISTINCT BContactID),
-		DedupedBuyers = COUNT(DISTINCT MasterContactID),
+		DedupedBuyers = COUNT(DISTINCT FS.MasterContactID),
 		Revenue = SUM(ExtRevenue),
 		AOV = SUM(ExtRevenue)/COUNT(DISTINCT OrderID)     
-FROM [dbo].[AGL_Fact_Sales_Base](NOLOCK)
-WHERE	YEAR(df_transactionDate) >= 2011
-GROUP BY	YEAR(df_transactionDate),
-			MONTH(df_transactionDate)
-			
+FROM		[dbo].[AGL_Fact_Sales_Base](NOLOCK) FS
+INNER JOIN	[dbo].[AGL_Dim_CustomerSummary_Base] (NOLOCK) C ON
+				FS.MasterContactID = C.MasterContactID
+WHERE	YEAR(df_transactionDate) >= 2014
+GROUP BY	[L12MDecile],
+			YEAR(df_transactionDate),
+			MONTH(df_transactionDate)			
